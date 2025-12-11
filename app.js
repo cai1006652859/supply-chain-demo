80934be5a06a090f1247745ee2248ad7e8f7165a
zwcai <zhongwen.cai@avepoint.com>
2025-12-11 21:12:36 +0800

    vt

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express()
 app.get('/ping', (req, res) => {
     password= 's3cr3t'    
     api_key="1234-1234-1234-1234"
+    api_key="1234-1234-1234-1234"
     const token = "ghp_123456789012345678901234567890123456"
 
     if(req.params.password === 's3cr3t'){
